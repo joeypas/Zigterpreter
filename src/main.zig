d5771f989f357ae41ebfc0299e20d5f2003b54f5
Joe Liotta <jliotta03@gmail.com>
2024-02-06 14:54:47 -0500

    AST

--- src/main.zig
+++ src/main.zig
@@ -16,11 +16,16 @@ const TType = enum {
     RPAREN,
 };
 
+const CharNum = union(enum) {
+    Char: u8,
+    Num: i64,
+};
+
 const Token = struct {
     Type: TType,
-    value: u8,
+    value: CharNum,
 
-    pub fn init(Ty: TType, val: u8) Token {
+    pub fn init(Ty: TType, val: CharNum) Token {
         return Token{
             .Type = Ty,
             .value = val,
@@ -32,11 +37,7 @@ const Token = struct {
     }
 };
 
-const TokenError = error{
-    InvalidToken,
-    InvalidCharacter,
-    InvalidType,
-};
+const TokenError = error{ InvalidToken, InvalidCharacter, InvalidType, Mem };
 
 const Lexer = struct {
     text: []const u8,
@@ -66,7 +67,7 @@ const Lexer = struct {
         }
     }
 
-    fn integer(self: *Lexer) !u8 {
+    fn integer(self: *Lexer) !i64 {
         const alloc = std.heap.page_allocator;
         var num = ArrayList(u8).init(alloc);
         defer num.deinit();
@@ -76,7 +77,7 @@ const Lexer = struct {
             self.advance();
         }
 
-        const ret = try std.fmt.parseInt(u8, num.items, 10);
+        const ret = try std.fmt.parseInt(i64, num.items, 10);
         return ret;
     }
 
@@ -89,37 +90,37 @@ const Lexer = struct {
 
             if (std.ascii.isDigit(self.current_char.?)) {
                 const num = self.integer() catch return TokenError.InvalidCharacter;
-                return Token.init(TType.INTEGER, num);
+                return Token.init(TType.INTEGER, .{ .Num = num });
             }
 
             if (self.current_char == '+') {
                 self.advance();
-                return Token.init(TType.PLUS, '+');
+                return Token.init(TType.PLUS, .{ .Char = '+' });
             }
 
             if (self.current_char == '-') {
                 self.advance();
-                return Token.init(TType.MINUS, '-');
+                return Token.init(TType.MINUS, .{ .Char = '-' });
             }
 
             if (self.current_char == '*') {
                 self.advance();
-                return Token.init(TType.MULTI, '*');
+                return Token.init(TType.MULTI, .{ .Char = '*' });
             }
 
             if (self.current_char == '/') {
                 self.advance();
-                return Token.init(TType.DIVIS, '/');
+                return Token.init(TType.DIVIS, .{ .Char = '/' });
             }
 
             if (self.current_char == '(') {
                 self.advance();
-                return Token.init(TType.LPAREN, '(');
+                return Token.init(TType.LPAREN, .{ .Char = '(' });
             }
 
             if (self.current_char == ')') {
                 self.advance();
-                return Token.init(TType.RPAREN, ')');
+                return Token.init(TType.RPAREN, .{ .Char = ')' });
             }
 
             return TokenError.InvalidToken;
@@ -129,22 +130,36 @@ const Lexer = struct {
     }
 };
 
-const Interpreter = struct {
+const Parser = struct {
     current_token: Token,
     lexer: *Lexer,
+    allocator: Allocator,
+    nodeList: ArrayList(*Tree(Token)),
 
-    pub fn init(lexer: *Lexer) !Interpreter {
-        return Interpreter{
+    pub fn init(alloc: Allocator, lexer: *Lexer) !Parser {
+        return Parser{
+            .allocator = alloc,
             .lexer = lexer,
             .current_token = try lexer.getNextToken(),
+            .nodeList = ArrayList(*Tree(Token)).init(alloc),
         };
     }
 
-    pub fn factor(self: *Interpreter) TokenError!u8 {
+    pub fn deinit(self: *Parser) void {
+        for (self.nodeList.items) |node| {
+            self.allocator.destroy(node);
+        }
+        self.nodeList.deinit();
+    }
+
+    pub fn factor(self: *Parser) TokenError!*Tree(Token) {
         const token = self.current_token;
         if (token.Type == TType.INTEGER) {
             try self.eat(TType.INTEGER);
-            return token.value;
+            const node = self.allocator.create(Tree(Token)) catch return TokenError.Mem;
+            node.* = Tree(Token){ .Data = token };
+            self.nodeList.append(node) catch return TokenError.Mem;
+            return node;
         } else if (token.Type == TType.LPAREN) {
             try self.eat(TType.LPAREN);
             const result = try self.expr();
@@ -154,26 +169,36 @@ const Interpreter = struct {
         return TokenError.InvalidToken;
     }
 
-    pub fn term(self: *Interpreter) TokenError!u8 {
+    pub fn term(self: *Parser) TokenError!*Tree(Token) {
         var result = try self.factor();
 
         while (self.current_token.Type == TType.MULTI or self.current_token.Type == TType.DIVIS) {
             const token = self.current_token;
             if (token.Type == TType.MULTI) {
                 try self.eat(TType.MULTI);
-                const part = try self.factor();
-                result = result * part;
             } else if (token.Type == TType.DIVIS) {
                 try self.eat(TType.DIVIS);
-                const part = try self.factor();
-                result = result / part;
             }
+
+            const part = try self.factor();
+            const node = self.allocator.create(Tree(Token)) catch return TokenError.Mem;
+            const branch = Tree(Token){
+                .Branch = .{
+                    .left = result,
+                    .right = part,
+                    .Data = token,
+                },
+            };
+            node.* = branch;
+            self.nodeList.append(node) catch return TokenError.Mem;
+
+            result = node;
         }
 
         return result;
     }
 
-    pub fn eat(self: *Interpreter, token_type: TType) TokenError!void {
+    pub fn eat(self: *Parser, token_type: TType) TokenError!void {
         if (self.current_token.Type == token_type) {
             self.current_token = try self.lexer.getNextToken();
         } else {
@@ -182,7 +207,7 @@ const Interpreter = struct {
         }
     }
 
-    pub fn expr(self: *Interpreter) TokenError!u8 {
+    pub fn expr(self: *Parser) TokenError!*Tree(Token) {
         // TODO: implement pemdas
         //
         var result = try self.term();
@@ -191,23 +216,86 @@ const Interpreter = struct {
             const token = self.current_token;
             if (token.Type == TType.PLUS) {
                 try self.eat(TType.PLUS);
-                const part = try self.term();
-                result = result + part;
             } else if (token.Type == TType.MINUS) {
                 try self.eat(TType.MINUS);
-                const part = try self.term();
-                result = result - part;
             }
+
+            const part = try self.term();
+            const node = self.allocator.create(Tree(Token)) catch return TokenError.Mem;
+            const branch = Tree(Token){
+                .Branch = .{
+                    .left = result,
+                    .right = part,
+                    .Data = token,
+                },
+            };
+            node.* = branch;
+            self.nodeList.append(node) catch return TokenError.Mem;
+            result = node;
         }
         return result;
+    }
+};
+
+pub fn Tree(comptime T: type) type {
+    return union(enum) {
+        Data: T,
+
+        Branch: struct {
+            left: *Tree(T),
+            right: *Tree(T),
+            Data: T,
+        },
+    };
+}
+
+const Interpreter = struct {
+    parser: *Parser,
+
+    pub fn init(parser: *Parser) Interpreter {
+        return Interpreter{
+            .parser = parser,
+        };
+    }
+
+    fn visit(self: *Interpreter, node: *Tree(Token)) i64 {
+        switch (node.*) {
+            .Branch => |branch| {
+                if (branch.Data.Type == TType.PLUS) {
+                    return self.visit(branch.left) + self.visit(branch.right);
+                } else if (branch.Data.Type == TType.MINUS) {
+                    return self.visit(branch.left) - self.visit(branch.right);
+                } else if (branch.Data.Type == TType.MULTI) {
+                    return self.visit(branch.left) * self.visit(branch.right);
+                } else if (branch.Data.Type == TType.DIVIS) {
+                    return @divTrunc(self.visit(branch.left), self.visit(branch.right));
+                } else {
+                    return 0;
+                }
+            },
+            .Data => |data| {
+                switch (data.value) {
+                    .Num => return data.value.Num,
+                    .Char => return 0,
+                }
+            },
+        }
+    }
+
+    pub fn interpret(self: *Interpreter) !i64 {
+        const tree = try self.parser.expr();
+        const res = self.visit(tree);
+        return res;
     }
 };
 
 test {
     const text = "(5+5)*5\n";
     var lex = Lexer.init(text);
-    var int = try Interpreter.init(&lex);
-    const result = try int.expr();
+    var parser = try Parser.init(std.testing.allocator, &lex);
+    defer parser.deinit();
+    var int = Interpreter.init(&parser);
+    const result = try int.interpret();
     try std.testing.expect(result == 50);
 }
 
@@ -236,9 +324,11 @@ pub fn main() !void {
                 break;
             }
             var lex = Lexer.init(text);
-            var int = try Interpreter.init(&lex);
+            var parser = try Parser.init(allocator, &lex);
+            defer parser.deinit();
+            var int = Interpreter.init(&parser);
 
-            const result = try int.expr();
+            const result = try int.interpret();
             try out.print("{d}\n", .{result});
         } else {
             continue;
