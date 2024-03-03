ec18ca673221fb777d4cdbc9185cecccae1c5887
Joe Liotta <jliotta03@gmail.com>
2024-03-03 14:48:52 -0500

    fix build temp

--- build.zig
+++ build.zig
@@ -31,19 +31,19 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const ast = b.addExecutable(.{
-        .name = "ast",
-        .root_source_file = .{ .path = "src/shell_ast.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+    //const ast = b.addExecutable(.{
+    //   .name = "ast",
+    //   .root_source_file = .{ .path = "src/shell_ast.zig" },
+    //   .target = target,
+    //   .optimize = optimize,
+    //});
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
     b.installArtifact(exe);
     b.installArtifact(gen);
-    b.installArtifact(ast);
+    //b.installArtifact(ast);
     // This *creates* a Run step in the build graph, to be executed when another
     // step is evaluated that depends on it. The next line below will establish
     // such a dependency.
